67001b751ac10b512ec43eb591b0ccf4ffb07b33
Hirenkumar Khant <hirenkumar.khant@ibm.com>
2021-05-07 16:39:30 -0300

    imported CustomerModule to AppModule and added code to AppComponentHtml

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppComponent } from './app.component';
-import { StoreModule } from '@ngrx/store';
 import { reducers, metaReducers } from './reducers';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
+import {CustomerModule} from './customer/customer.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { environment } from '../environments/environment';
   imports: [
     BrowserModule,
     StoreModule.forRoot(reducers, { metaReducers }),
-    !environment.production ? StoreDevtoolsModule.instrument() : []
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    CustomerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
