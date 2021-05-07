66974f55efb93638ac64341a9fec3798263c44cd
Hirenkumar Khant <hirenkumar.khant@ibm.com>
2021-05-07 15:58:28 -0300

    generated Customer Reducer customer.reducer: created customerReducer and a function reducer customer.actions: created addCustomer action

--- src/app/customer/store/action/customer.actions.ts
+++ src/app/customer/store/action/customer.actions.ts
@@ -1,9 +1,7 @@
 import { createAction, props } from '@ngrx/store';
+import { Customer } from '../../../models/customer';
 
-export const loadCustomers = createAction(
-  '[Customer] Load Customers'
+export const addCustomer = createAction(
+  '[Customer] Add Customer',
+    (customer: Customer) => ({ customer })
 );
-
-
-
-
