b5f5a2255672f4624f316774c6c07ef30ce04404
Hirenkumar Khant <hirenkumar.khant@ibm.com>
2021-05-07 16:20:12 -0300

    added code to display customers customerViewComponent: created a customer[] observable using the model 	using the reducer state created a store and used selector to populate the customers obervable customerViewHtml: added code to list customers from customers$

--- src/app/customer/customer-view/customer-view.component.ts
+++ src/app/customer/customer-view/customer-view.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+// Declare the customers that are observable at the top of the class and 
+// modify the constructor and use the selector to get Observable of Customers
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { select, Store } from '@ngrx/store';
+
+import { Customer } from '../../models/customer';
+import { selectCustomers } from '../store/selector/customer.selectors';
+import { CustomerState } from '../store/reducer/customer.reducer';
 
 @Component({
   selector: 'app-customer-view',
   templateUrl: './customer-view.component.html',
   styleUrls: ['./customer-view.component.scss']
 })
-export class CustomerViewComponent implements OnInit {
+export class CustomerViewComponent {
 
-  constructor() { }
+  customers$: Observable<Customer[]>;
 
-  ngOnInit(): void {
+  constructor(private store: Store<CustomerState>) {
+    this.customers$ = this.store.pipe(select(selectCustomers));
   }
 
 }
